88ad8e2a74e13f2d1068d5a01619c876319174ce
Hector Romero <hector.romero@sopristec.com>
2026-09-22 17:52:53 -0600

    Adde unsupported media type error handling and  method not allowed method handling

--- app/errors.py
+++ app/errors.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from marshmallow import ValidationError
+from werkzeug.exceptions import UnsupportedMediaType, MethodNotAllowed
 
 class JobNotFoundError(Exception):
     pass
@@ -20,3 +21,17 @@ def register_error_handlers(app):
             "error": "job_not_found",
             "message" : "Job not Found."
         }), 404
+        
+    @app.errorhandler(UnsupportedMediaType)
+    def handle_unsupported_media_type(error):
+        return jsonify({
+            "error": "unsupported_media_type",
+            "message": "Content-Type must be application/json."
+        }), 415
+    
+    @app.errorhandler(MethodNotAllowed)
+    def handle_method_not_allowed(error):
+            return jsonify({
+                "error": "method_not_allowed",
+                "message" : "This HTTP method is not allowed for this resource."
+            }), 405
